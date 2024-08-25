846415dac78a8bb47ad66cbd6848e1d2a8e08ee4
abdelrahman2206 <128705718+abdelrahman2206@users.noreply.github.com>
2024-08-25 11:21:14 +0300

    Add files via upload

---
+++ services_new.aspx
@@ -0,0 +1,62 @@
+﻿<%@ Page Title="services_new" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="services_new.aspx.cs" Inherits="services_new.About" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+   &nbsp;<html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><link rel="stylesheet" href="services_new.css"><title>Document</title></head><body><div id="main">
+        <div id="title">
+            <h3>System</h3>
+        </div>
+        <div id="Logo"></div>
+        <div id="bloc1">
+            
+           <asp:TextBox ID="Mname" runat="server" Placeholder="Name"></asp:TextBox>
+            <asp:DropDownList ID="MType" runat="server">
+                <asp:ListItem Text="" Value="new0"></asp:ListItem>
+                <asp:ListItem Text="EGP" Value="new1"></asp:ListItem>
+                <asp:ListItem Text="USD" Value="new2"></asp:ListItem>
+                <asp:ListItem Text="EURO" Value="new3"></asp:ListItem>
+                <asp:ListItem Text="RSA" Value="new4"></asp:ListItem>
+            </asp:DropDownList>
+            
+            
+            <asp:TextBox ID="Mqty" runat="server" TextMode="Number" Placeholder="Quantity"></asp:TextBox>
+            
+            <asp:TextBox ID="Mprice" runat="server" TextMode="Number" Placeholder="Price"></asp:TextBox>
+            <asp:DropDownList ID="Manu" runat="server"  >
+                <asp:ListItem Text="" Value="new0"></asp:ListItem>
+                <asp:ListItem Text="service_1" Value="new1"></asp:ListItem>
+                <asp:ListItem Text="service_2" Value="new2"></asp:ListItem>
+                <asp:ListItem Text="service_3" Value="new3"></asp:ListItem>
+                <asp:ListItem Text="service_4" Value="new4"></asp:ListItem>
+            </asp:DropDownList>
+
+            
+            
+        </div>
+        <br /><br />
+        <div id="Bt_block1">
+             <asp:Button ID="Editbtn" runat="server" Text="Edit" Height="30px" Width="94px" OnClick="Editbtn_Click" /> <button runat="server">Delete</button>
+            <asp:Button ID="addbtn" runat="server" Text="Add" OnClick="Addbtn_Click" Height="30px" Width="94px" /> 
+             </div>
+        <br /> <br />
+        <div id="grid_table">
+
+            <asp:GridView id="GV1" runat="server" AutoGenerateSelectButton="True" CellPadding="4"  ForeColor="#333333" GridLines="None" OnSelectedIndexChanged="GV1_SelectedIndexChanged" >
+                <AlternatingRowStyle BackColor="White" />
+                <EditRowStyle BackColor="#7C6F57" />
+                <FooterStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <HeaderStyle BackColor="#1C5E55" Font-Bold="True" ForeColor="White" />
+                <PagerStyle BackColor="#666666" ForeColor="White" HorizontalAlign="Center" />
+                <RowStyle BackColor="#E3EAEB" />
+                <SelectedRowStyle BackColor="#C5BBAF" Font-Bold="True" ForeColor="#333333" />
+                <SortedAscendingCellStyle BackColor="#F8FAFA" />
+                <SortedAscendingHeaderStyle BackColor="#246B61" />
+                <SortedDescendingCellStyle BackColor="#D4DFE1" />
+                <SortedDescendingHeaderStyle BackColor="#15524A" />
+            </asp:GridView>
+
+        </div>
+</div>
+</body>
+
+</html>
+</asp:Content>
---
+++ services_new.aspx.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace services_new
+{
+    
+    public partial class About : Page
+        
+    {
+        private int key = 0;
+        private SqlConnection con = new SqlConnection(@"Data Source=localhost\sqlexpress;Integrated Security=True");
+        private void Showgrid()
+        {
+            con.Open();
+            string  Quiry = "select * from services_new ";
+            SqlDataAdapter sda= new SqlDataAdapter(Quiry,con);
+            
+            var ds = new DataSet();
+            sda.Fill(ds);
+            GV1.DataSource = ds.Tables[0];
+            GV1.DataBind();
+
+
+            con.Close();    
+        }
+          
+
+        private void Editserv()
+        {
+            if (Mname.Text == "" || MType.SelectedIndex == -1 || Mqty.Text == "" || Mprice.Text == "" || Manu.SelectedIndex == -1)
+            {
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", "alert('missing data');", true);
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE services_new SET name = @N,type = @T,quantity = @Q,price = @P,manufacture=@M WHERE num=@W", con);
+                    cmd.Parameters.AddWithValue("@N", Mname.Text);
+                    cmd.Parameters.AddWithValue("@T", MType.SelectedItem.ToString());
+                    cmd.Parameters.AddWithValue("@Q", Mqty.Text);
+                    cmd.Parameters.AddWithValue("@P", Mprice.Text);
+                    cmd.Parameters.AddWithValue("@M", Manu.SelectedItem.ToString());
+                    cmd.Parameters.AddWithValue("@W", key);
+                    cmd.ExecuteNonQuery();
+                    ScriptManager.RegisterStartupScript(this,this.GetType(),"Script","alert('services updated in stock');",true);
+                    con.Close();
+                    Showgrid();
+
+                }
+                catch
+                {
+
+                    throw;
+                }
+            }
+        }
+        private void InsertName()
+        {
+            if (Mname.Text == "" || MType.SelectedIndex == -1 || Mqty.Text == "" || Mprice.Text == "" || Manu.SelectedIndex == -1)
+            {
+                Console.WriteLine("incomplete data");
+            }
+            else
+            {
+                try
+                {
+                    con.Open();
+                    SqlCommand cmd = new SqlCommand("insert into services_new(name,type,quantity,price,manufacture) values(@N,@T,@Q,@P,@M)", con);
+                    cmd.Parameters.AddWithValue("@N", Mname.Text);
+                    cmd.Parameters.AddWithValue("@T", MType.SelectedItem.Text);
+                    cmd.Parameters.AddWithValue("@Q", Mqty.Text);
+                    cmd.Parameters.AddWithValue("@P", Mprice.Text);
+                    cmd.Parameters.AddWithValue("@M", Manu.SelectedItem.Text);
+                    cmd.ExecuteNonQuery();
+                    //ScriptManager.RegisterStartupScript(this,this.GetType(),"Script","alert('services Added in stock');",true);
+                    con.Close();
+                    Showgrid();
+
+                }
+                catch
+                {
+
+                 throw;
+                }
+            }
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Showgrid();
+        }
+
+        protected void Addbtn_Click(object sender, EventArgs e)
+        { 
+         InsertName();
+        }
+        
+        protected void GV1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
+            Mname.Text = GV1.SelectedRow.Cells[2].Text;
+            MType.SelectedItem.Text = GV1.SelectedRow.Cells[3].Text;
+            Mqty.Text = GV1.SelectedRow.Cells[4].Text;
+            Mprice.Text = GV1.SelectedRow.Cells[5].Text;
+            Manu.SelectedItem.Text = GV1.SelectedRow.Cells[6].Text;
+
+            
+            //ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", $"alert('Key: {key}, Name: {Mname.Text}');", true);
+            //ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", "alert('M.name');", true);
+        }
+
+        protected void Editbtn_Click(object sender, EventArgs e)
+        {
+            if (Mname.Text == "")
+            {
+                key = 0;
+            }
+            else
+            {
+                key = Convert.ToInt32(GV1.SelectedRow.Cells[1].Text);
+            }
+
+            Editserv();
+           // ScriptManager.RegisterStartupScript(this, this.GetType(), "Script", $"alert('Key: {key}, Name: {Mname.Text}');", true);
+            
+        }
+    }
+}
